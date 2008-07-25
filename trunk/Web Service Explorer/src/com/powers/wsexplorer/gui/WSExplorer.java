938d21ecc2f1acf048397e49c52f05b31cb58332
powers.nick <powers.nick@eb22e197-3c51-0410-a734-3fb14a677ee6>
2008-07-25 19:21:42 +0000

    Organized imports.

--- trunk/Web Service Explorer/src/com/powers/wsexplorer/gui/WSExplorer.java
+++ trunk/Web Service Explorer/src/com/powers/wsexplorer/gui/WSExplorer.java
@@ -46,7 +46,6 @@ import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.grouplayout.GroupLayout;
 import org.eclipse.swt.layout.grouplayout.LayoutStyle;
