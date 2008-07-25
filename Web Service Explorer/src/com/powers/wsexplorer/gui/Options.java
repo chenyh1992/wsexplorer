c5683266dbfc475ae1f91f13716accfb29c01a83
powers.nick <powers.nick@eb22e197-3c51-0410-a734-3fb14a677ee6>
2008-07-25 19:09:40 +0000

    -New functionality to statusBar (pop-up menu) including clearing it and seeing the full stack trace -Added an option to not pay attention to certificates in HTTPS (in case of no certificate or wrong certificate) -

---
+++ Web Service Explorer/src/com/powers/wsexplorer/gui/Options.java
@@ -0,0 +1,7 @@
+package com.powers.wsexplorer.gui;
+
+public class Options {
+
+	public boolean ignoreHostCertificates;
+	public static final String IGNORE_HOST_CERTIFICATS_KEY = "ignore.host.certificates";
+}
